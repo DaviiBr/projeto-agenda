26c40e315ff1195a70b8d0584a3890bb0ec0eedb
Davi Brasil <davibrclau@gmail.com>
2024-02-16 14:48:24 -0300

    first commit

---
+++ frontend/main.js
@@ -0,0 +1,6 @@
+import 'core-js/stable'; // biblioteca Js, para suportar em navegadores antigos
+import 'regenerator-runtime/runtime'; // dependencia do babel, para auxiliar em funcoes assincronas
+
+import './assets/css/style.css'; // carrega o css no site
+
+console.log('Olá mundo 3'); // ola mundo ne po
