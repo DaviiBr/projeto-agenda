6e34449433acb11fe1e78e20ae60f5d79e93babc
Davi Brasil <davibrclau@gmail.com>
2024-02-22 09:48:18 -0300

    Read and delete contatos

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -38,7 +38,7 @@ exports.editIndex = async function(req, res) {
 
 exports.edit = async function(req, res) {
     try{
-        
+
         if(!req.params.id) return res.render('404')
         const contato = new Contato(req.body)
         await contato.edit(req.params.id)
@@ -58,4 +58,15 @@ exports.edit = async function(req, res) {
         res.render('404')
     }
     
+}
+
+exports.delete = async function(req, res) {
+    if(!req.params.id) return res.render('404')
+
+    const contato = await Contato.delete(req.params.id)
+    if(!contato) return res.render('404')
+
+    req.flash('success', 'Contato apagado com sucesso.')
+    req.session.save(() => res.redirect(`/`))
+    return
 }
