4d674859f6c340195c0dbc1ca04610bcddd913af
Davi Brasil <davibrclau@gmail.com>
2024-02-23 14:47:06 -0300

    Validando front-end

--- frontend/main.js
+++ frontend/main.js
@@ -1,6 +1,9 @@
 import 'core-js/stable'; // biblioteca Js, para suportar em navegadores antigos
 import 'regenerator-runtime/runtime'; // dependencia do babel, para auxiliar em funcoes assincronas
 
-import './assets/css/style.css'; // carrega o css no site
+import Login from './modules/Login'
 
-console.log('Olá mundo 3'); // ola mundo ne po
+const login = new Login('.form-login')
+const cadastro = new Login ('.form-cadastro')
+login.init()
+cadastro.init()
